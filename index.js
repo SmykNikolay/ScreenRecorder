295919bd16c26fce8c6dc7ac2284af1c14d90eb2
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-09-26 17:33:27 +0300

    Refactor code to use dynamic file name for final merged output

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const Recorder = require("./Recorder");
-// const Merger = require("./Merger");
 const schedule = require("node-schedule");
+
+const VIDEO_SIZE = "1280x720";
+const SCREEN_SIZE = "1920x1080";
+const FRAME_RATE = 30;
 
 const videoRecorder = new Recorder(
   "video",
   (outputFilePath) =>
-    `ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "0" -t 2 -c:v libx264 ${outputFilePath}`,
+    `ffmpeg -f avfoundation -framerate ${FRAME_RATE} -video_size ${VIDEO_SIZE} -i "0" -t 2 -c:v libx264 ${outputFilePath}`,
 );
+
 const screenRecorder = new Recorder(
   "screen",
   (outputFilePath) =>
-    `ffmpeg -f avfoundation -pix_fmt uyvy422 -framerate 30 -video_size 1920x1080 -probesize 100M -i "3:none" -t 2 -c:v libx264 -preset fast -crf 23 -level 4.1 ${outputFilePath}`,
+    `ffmpeg -f avfoundation -pix_fmt uyvy422 -framerate ${FRAME_RATE} -video_size ${SCREEN_SIZE} -probesize 100M -i "3:none" -t 2 -c:v libx264 -preset fast -crf 23 -level 4.1 ${outputFilePath}`,
 );
 
-const screenshotRecorder = new Recorder(
-  "screen",
-  (outputFilePath) =>
-    `ffmpeg -f avfoundation -framerate 1 -video_size 1920x1080 -i "3:none" -vframes 1 ${outputFilePath}`,
-);
-
-const cameraScreenshotRecorder = new Recorder(
-  "camera",
-  (outputFilePath) =>
-    `ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "0:none" -vframes 1 ${outputFilePath}`,
-);
-
-// const merger = new Merger();
-schedule.scheduleJob("* * * * *", () => videoRecorder.record());
-schedule.scheduleJob("* * * * *", () => screenRecorder.record());
-// schedule.scheduleJob("* * * * *", () => screenshotRecorder.capture());
-// schedule.scheduleJob("* * * * *", () => cameraScreenshotRecorder.capture());
-
-// schedule.scheduleJob("*/10 * * * *", () => merger.mergeVideos());
-// schedule.scheduleJob("* * * * *", () => merger.mergeAllMergedVideos());
+schedule.scheduleJob("* * * * *", () => {
+  try {
+    videoRecorder.record();
+    screenRecorder.record();
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+});
